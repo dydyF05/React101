d45f73415ca2de3b38a5af92df3fee7c1e3d5634
dylan <dylanfoster.esgi@gmail.com>
2017-01-03 16:23:10 +0100

    first state changes provoqued and transmitted from one component to another

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
 class App extends React.Component {
 	constructor(props) {
@@ -52,24 +53,77 @@ class Header extends React.Component {
 }
 
 class Content extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: 0,
+			inputVal: "chibar"
+		}
+		this.setNewNumber = this.setNewNumber.bind(this)
+		this.forceUpdateHandler = this.forceUpdateHandler.bind(this)
+		this.inputChangeHandler = this.inputChangeHandler.bind(this)
+	}
+	componentWillMount() {
+		console.log('Content WILL MOUNT!')
+	}
+
+	componentDidMount() {
+		console.log('Content DID MOUNT!')
+	}
+
+	componentWillReceiveProps(newProps) {    
+		console.log('Content WILL RECIEVE PROPS!')
+	}
+
+	shouldComponentUpdate(newProps, newState) {
+		return true;
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		console.log('Content WILL UPDATE!');
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log('Content DID UPDATE!')
+	}
+
+	componentWillUnmount() {
+		console.log('Content WILL UNMOUNT!')
+	}
+	setNewNumber() {
+		this.setState({data: this.state.data + 1})
+	}	
+	forceUpdateHandler() {
+		this.forceUpdate()
+	}
+	inputChangeHandler(e) {
+		this.setState({inputVal: e.target.value})
+	}
 	render() {
 		const style = {
 			fontSize: 35,
-			color: "skyblue"
+			color: "skyblue",
+			border: "1px solid #C09F03"
 		}
 		return (
 			<div>
-				<h2 style={style} className="helloClass">Subtitle {4%2}</h2>
-				<p data-myAttr="test">Loreum whatever</p>
+				<h2 onClick={this.setNewNumber} style={style} className="helloClass">val: {this.state.inputVal}</h2>
+				<input type="text" value={this.state.inputVal} onChange={this.inputChangeHandler}/>
+				<p id="inputView">Loreum whatever</p>
+				<h5>Random number: {Math.random()}</h5>
+				<button onClick={this.forceUpdateHandler}>Force update</button>
 				<DTable/>
+				<ChangingDiv
+					myNumber = {this.state.data}
+				/>
 			</div>
 		);
 	}
 }
 
 class DTable extends React.Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 
 		this.state = {
 			data: [
@@ -90,21 +144,33 @@ class DTable extends React.Component {
 				}
 			]
 		}
+		this.setStateHandler = this.setStateHandler.bind(this);
 	}
+
+	setStateHandler() {
+		const item = {id: 5, name: "hello moto", age: 99}
+		const arr = this.state.data
+		arr.push(item)
+		this.setState({data: arr})
+	}
+	
 	render() {
 		return (
-			<table>
-				<thead>
-					<tr>
-						<th>id</th>
-						<th>name</th>
-						<th>age</th>
-					</tr>
-				</thead>
-				<tbody>
-					{this.state.data.map( (p, i) => <DTableRow key={i} data={p}/> )}
-				</tbody>
-			</table>
+			<div>
+				<table>
+					<thead>
+						<tr>
+							<th>id</th>
+							<th>name</th>
+							<th>age</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.data.map( (p, i) => <DTableRow key={i} data={p}/> )}
+					</tbody>
+				</table>
+				<button onClick={this.setStateHandler}>Set State</button>
+			</div>
 		)
 	}
 }
@@ -117,6 +183,36 @@ class DTableRow extends React.Component {
 				<td>{this.props.data.name}</td>
 				<td>{this.props.data.age}</td>
 			</tr>
+		)
+	}
+}
+
+class ChangingDiv extends React.Component {
+	constructor(props) {
+		super(props);
+		this.findDomHandler = this.findDomHandler.bind(this)
+	}
+	componentWillReceiveProps({myNumber}) {    
+		console.log('ChangingDiv WILL RECIEVE PROPS!');
+		const d = document.getElementById("changingDiv");
+		const c = ReactDom.findDOMNode(d);
+		c.innerHTML = `Content state data received in ChangingDiv: ${myNumber}`;
+	}
+	findDomHandler() {
+		const d = document.getElementById("changingDiv");
+		const c = ReactDom.findDOMNode(d).style.background;
+		ReactDom.findDOMNode(d).style.background = ( c === "skyblue" ) ? "green" : "skyblue";
+	}
+	render() {
+		const style = {
+			height: 100,
+			width: 300,
+			background: "skyblue"
+		}
+		return (
+			<div>
+				<div id="changingDiv" onClick={this.findDomHandler} style={style}></div>
+			</div>
 		)
 	}
 }
